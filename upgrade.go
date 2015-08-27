7c57f601283c8367c46881a88a7be672db1d3a04
Alexandre Thomas <alexandre_thomas@calypso>
2015-08-27 11:34:10 +0200

    Add line number to upgrade.

--- upgrade.go
+++ upgrade.go
@@ -5,17 +5,19 @@ var (
 )
 
 type Upgrade interface {
-	Mark string
-	Name string
-	Cost string
-	Line int
+}
+
+type RawUpgrade struct {
+	mark string
+	name string
+	cost string
+	line	int
 }
 
 // ParseUpgrade generate an upgrade from a raw line
-func ParseUpgrade(raw string, line int) (Upgrade, error) {
-	// Initialize a new upgrade
-	upgrade := Upgrade{
-		Line: line,
+func ParseUpgrade(line int, raw string) (RawUpgrade, error) {
+	upgrade := RawUpgrade{
+		line: line,
 	}
 	
 	// Get the fields of the line
@@ -23,16 +25,16 @@ func ParseUpgrade(raw string, line int) (Upgrade, error) {
 	
 	// The minimum number of fields is 2
 	if len(fields) < 2 {
-		return Upgrade{}, fmt.Errorf("not enought")
+		return upgrade, fmt.Errorf("Error on line %d: expected at least mark and label.", line)
 	}
 	
 	// Check that the mark is a valid one
 	if !in(fields[0], []string{"*", "+", "-"}) {
-		return Upgrade{}, fmt.Errorf("%s isn't a valid mark", fields[0])
+		return upgrade, fmt.Errorf("Error on line %d: %s is not a valid mark", line, fields[0])
 	}
 	
 	// Set the upgrade mark
-	upgrade.Mark = fields[0]
+	upgrade.mark = fields[0]
 	fields = fields[1:]
 	
 	// Check if a field seems to be a cost field
@@ -41,13 +43,13 @@ func ParseUpgrade(raw string, line int) (Upgrade, error) {
 			continue
 		}
 		
-		upgrade.Cost = regex_xp.FindString(field)
+		upgrade.cost = regex_xp.FindString(field)
 		fields = append(fields[:i], fields[i+1:]...)
 		break
 	}
 	
 	// The remaining line is the name of the upgrade
-	upgrade.Name = strings.Join(fields, " ")
+	upgrade.name = strings.Join(fields, " ")
 	
 	return upgrade, nil
 }
