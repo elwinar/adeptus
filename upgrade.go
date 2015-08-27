868bfc9e328868ba9c58b033648ebdd8ff64f9d1
Alexandre Thomas <alexandre_thomas@calypso>
2015-08-27 12:25:56 +0200

    Adds tests for upgrade.go

--- upgrade.go
+++ upgrade.go
@@ -1,7 +1,7 @@
 package adeptus
 
 var (
-	regex_xp = regexp.MustCompile(`\(?\d+xp\)?`) // Match `150xp` and `(150xp)`
+	regex_xp = regexp.MustCompile(`(?i)\(?\d+xp\)?`) // Match `150xp`, `150XP` and `(150xp)`
 )
 
 type Upgrade interface {
@@ -30,25 +30,33 @@ func ParseUpgrade(line int, raw string) (RawUpgrade, error) {
 	
 	// Check that the mark is a valid one
 	if !in(fields[0], []string{"*", "+", "-"}) {
-		return upgrade, fmt.Errorf("Error on line %d: %s is not a valid mark", line, fields[0])
+		return upgrade, fmt.Errorf("Error on line %d: \"%s\" is not a valid mark (\"*\", \"+\", \"-\").", line, fields[0])
 	}
 	
 	// Set the upgrade mark
-	upgrade.mark = fields[0]
+	mark := fields[0]
 	fields = fields[1:]
 	
 	// Check if a field seems to be a cost field
+	var cost string
 	for i, field := range fields {
 		if !regex_xp.MatchString(field) {
 			continue
 		}
 		
-		upgrade.cost = regex_xp.FindString(field)
+		cost = regex_xp.FindString(field)
 		fields = append(fields[:i], fields[i+1:]...)
 		break
 	}
 	
 	// The remaining line is the name of the upgrade
+	if len(fields) == 0 {
+		return upgrade, fmt.Errorf("Error on line %d: name should not be empty.", line)
+	}
+	
+	// Set the upgrade attributes at the end to return empty upgrade in case of error
+	upgrade.mark = mark
+	upgrade.cost = cost
 	upgrade.name = strings.Join(fields, " ")
 	
 	return upgrade, nil
---
+++ upgrade_test.go
@@ -0,0 +1,212 @@
+package adeptus
+
+import(
+	"testing"
+)
+
+func Test_ParseUpgrade(t *testing.T) {
+	cases := []struct{
+		in string
+		line int
+		out RawUpgrade
+		err	bool
+	}{
+		{
+			in: "",
+			line: 1,
+			out: RawUpgrade{line: 1},
+			err: true,
+		},
+		{
+			in: "	",
+			line: 1,
+			out: RawUpgrade{line: 1},
+			err: true,
+		},
+		{
+			in: " 	 ",
+			line: 1,
+			out: RawUpgrade{line: 1},
+			err: true,
+		},
+		{
+			in: "fail",
+			line: 1,
+			out: RawUpgrade{line: 1},
+			err: true,
+		},
+		{
+			in: "x fail",
+			line: 1,
+			out: RawUpgrade{line: 1},
+			err: true,
+		},
+		{
+			in: "* 250xp",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				
+			},
+			err: true,
+		},
+		{
+			in: "* success",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				
+			},
+			err: false,
+		},
+		{
+			in: "* experience success",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "experience success",
+			},
+			err: false,
+		},
+		{
+			in: "* 250XP success",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* 250xp success",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "	* 250xp success",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success 250xp",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success (250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success +4 (250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success +4",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success	+4	(250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success +4",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success: confirmed	(250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success: confirmed",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: " * success - confirmed	(250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success - confirmed",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: "* success (confirmed)	(250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success (confirmed)",
+				cost: "250"
+			},
+			err: false,
+		},
+		{
+			in: " *	success	(confirmed)	(250xp)",
+			line: 1,
+			out: RawUpgrade{
+				line: 1,
+				mark: "*",
+				name: "success	(confirmed)",
+				cost: "250"
+			},
+			err: false,
+		},
+	}
+	
+	for i, c := range cases {
+		out, err := ParseUpgrade(c.in, c.line)
+		if (err != nil) != c.err {
+			t.Logf("Unexpected error on case %d:", i+1)
+			t.Logf("	Having %s", err)
+			t.Fail()
+			continue
+		}
+		if out != c.out {
+			t.Logf("Unexpected output on case %d:", i+1)
+			t.Logf("	Expected %v", c.out)
+			t.Logf("	Having %v", out)
+			t.Fail()
+		}
+	}
+}
