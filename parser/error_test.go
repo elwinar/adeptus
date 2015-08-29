c585cd85010de0cd58ef8059052b21ac605e6f95
Romain Baugue <romain.baugue@elwinar.com>
2015-08-29 12:17:50 +0200

    Stub tests for the error type

---
+++ parser/error_test.go
@@ -0,0 +1,13 @@
+package parser
+
+import (
+	"testing"
+)
+
+func Test_NewError(t *testing.T) {
+	t.Fatalf("unimplemented")
+}
+
+func Test_Error_Error(t *testing.T) {
+	t.Fatalf("unimplemented")
+}
