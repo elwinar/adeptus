a1e1099609bea50864f30ed515db0a670385ae43
Romain Baugue <romain.baugue@elwinar.com>
2015-08-29 13:33:21 +0200

    Implement parser.Error tests

--- parser/error_test.go
+++ parser/error_test.go
@@ -5,9 +5,24 @@ import (
 )
 
 func Test_NewError(t *testing.T) {
-	t.Fatalf("unimplemented")
+	err := NewError(0, EmptySheet)
+	
+	if err.Line != 0 {
+		t.Logf("invalid line number: expected %d, got %d", 0, err.Line)
+		t.Fail()
+	}
+	
+	if err.Code != EmptySheet {
+		t.Logf("invalid code: expected %d, got %d", EmptySheet, err.Code)
+		t.Fail()
+	}
 }
 
 func Test_Error_Error(t *testing.T) {
-	t.Fatalf("unimplemented")
+	err := NewError(0, EmptySheet)
+	
+	if err.Error() != "line 0: 0" {
+		t.Logf("invalid output: %s", err.Error())
+		t.Fail()
+	}
 }
