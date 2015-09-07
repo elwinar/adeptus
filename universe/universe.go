d628c6558e35c3fa95cde956bb8327b58fe0f619
Romain Baugue <romain.baugue@elwinar.com>
2015-09-07 20:03:15 +0200

    Add comments and format

--- universe/universe.go
+++ universe/universe.go
@@ -1,5 +1,11 @@
 package universe
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
 type Universe struct {
 	Origins         []Origin
 	Backgrounds     []Background
@@ -12,17 +18,18 @@ type Universe struct {
 	Talents         []Talent
 }
 
+// ParseUniverse load an universe from a plain JSON file. It returns a well-formed universe that describe all the components of a game setting.
 func ParseUniverse(file io.Reader) (Universe, error) {
 	raw, err := ioutil.ReadAll(file)
 	if err != nil {
 		return Universe{}, fmt.Errorf("unable to read universe: %s", err.Error())
 	}
-	
+
 	universe := Universe{}
 	err := json.Unmarshall(file, &universe)
 	if err != nil {
 		return Universe{}, fmt.Errorf("unable to parse universe: %s", err.Error())
 	}
-	
+
 	return universe, nil
 }
