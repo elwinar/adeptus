2c6dc6848618e8156daf0bbf48178b001dd37375
Alexandre Thomas <alexandre.thomas@outlook.fr>
2015-09-18 21:49:37 +0200

    Add NewXX methods to background, origin, role and tarot.

--- universe/origin.go
+++ universe/origin.go
@@ -6,3 +6,8 @@ type Origin struct {
 	Name string
 	Upgrades [][]parser.Upgrade
 }
+
+// NewOrigin returns the origin associated to the name
+func NewOrigin(name string) Origin {
+		return Origin{}
+}
--- universe/role.go
+++ universe/role.go
@@ -6,3 +6,8 @@ type Role struct {
 	Name string
 	Upgrades [][]parser.Upgrade
 }
+
+// NewRole returns the role associated to the name
+func NewRole(name string) Role {
+		return Role{}
+}
