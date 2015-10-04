edad394abb070ec7244ea2232c66a51305a43b3c
Alexandre Thomas <alexandre.thomas@outlook.fr>
2015-10-04 14:26:10 +0200

    WIP

--- error.go
+++ error.go
@@ -4,125 +4,83 @@ package main
 type ErrorCode int
 
 const (
-	// InsuficientData is returned when the sheet does not contain the minimal two mandatory blocks.
-	InsuficientData ErrorCode = 000
-
-	// InvalidKeyValuePair is returned when a header line isn't of the right format.
-	InvalidKeyValuePair ErrorCode = 100
-
-	// EmptyMetaKey is returned when the meta has an empty key.
-	EmptyMetaKey ErrorCode = 101
-
-	// EmptyMetaValue is returned when the meta has an empty value.
-	EmptyMetaValue ErrorCode = 102
-
-	// InvalidOptions is returned when the options provided are not allowed.
-	InvalidOptions ErrorCode = 103
-
-	// DuplicateMeta is returned when a meta is specified more than once.
-	DuplicateMeta ErrorCode = 104
-
-	// NoDate is returned when no date of the right format is found in the headline.
-	NoDate ErrorCode = 200
-
-	// InvalidReward is returned when the reward of a headline isn't correctly formed.
-	InvalidReward ErrorCode = 201
-
-	// RewardAlreadyFound is returned when two or more rewards are found for the same headline.
-	RewardAlreadyFound ErrorCode = 202
-
-	// WrongRewardPosition is returned when the reward isn't on the second or last position of a headline.
-	WrongRewardPosition ErrorCode = 203
-
-	// InvalidUpgrade is returned when the upgrade format is invalid.
-	InvalidUpgrade ErrorCode = 300
-
-	// InvalidMark is returned when the mark of an upgrade isn't recognized.
-	InvalidMark ErrorCode = 301
-
-	// InvalidCost is returned when the cost of an upgrade isn't correctly formed.
-	InvalidCost ErrorCode = 302
-
-	// CostAlreadyFound is returned when two or more costs are found for the same upgrade.
-	CostAlreadyFound ErrorCode = 303
-
-	// WrongCostPosition is returned when the cost isn't on the second or last position of an upgrade.
-	WrongCostPosition ErrorCode = 304
-
-	// EmptyName is returned when the name of an upgrade is empty.
-	EmptyName ErrorCode = 305
-
-	// InvalidCharacteristicFormat is returned when the characteristic format is incorrect.
-	InvalidCharacteristicFormat ErrorCode = 400
-
-	// NotIntegerCharacteristicValue is returned when the characteristic value is not a positive integer.
-	NotIntegerCharacteristicValue ErrorCode = 401
-	
-	// UnusedAptitude is returned when an aptitude is defined in the universe but not used.
-	UnusedAptitude ErrorCode = 500
-	
-	// UndefinedAptitude is returned when an non-defined aptitude is used by the universe.
-	UndefinedAptitude ErrorCode = 501
-	
-	// UndefinedBackground is returned when a character uses a non defined backgroud.
-	UndefinedBackground ErrorCode = 502
-	
-	// UndefinedCharacteristic is returned when a character uses a non defined characteristic.
-	UndefinedCharacteristic = 503
-	
-	// DuplicateCharacteristic is returned when a characteristic is found twice for a character
-	DuplicateCharacteristic = 504
-	
-	// MissingCharacteristic is returned when a characteristic is missing from a character's sheet.
-	MissingCharacteristic = 505
+    
+        UnusedAptitude ErrorCode =                000
+        UndefinedAptitude ErrorCode =             001
+    
+	InvalidCharacterSheet ErrorCode =         100
+        
+	InvalidHeaderLine ErrorCode =             200
+	EmptyHeaderKey ErrorCode =                201
+	EmptyHeaderValue ErrorCode =              202
+	DuplicateBackground ErrorCode =           203
+	InvalidBackgroundOptions ErrorCode =      204
+        UndefinedBackgroundType ErrorCode =       205
+        UndefinedBackgroundValue ErrorCode =      206
+        
+	UndefinedSessionDate ErrorCode =          300
+	InvalidSessionReward ErrorCode =          301
+	DuplicateSessionReward ErrorCode =        302
+	BadSessionRewardPosition ErrorCode =      303
+        
+	InvalidUpgrade ErrorCode =                400
+	UndefinedUpgradeName ErrorCode =          401
+	InvalidUpgradeMark ErrorCode =            402
+	InvalidUpgradeCost ErrorCode =            403
+	DuplicateUpgradeCost ErrorCode =          404
+	BadUpgradeCostPosition ErrorCode =        405
+        MismatchMarkCost ErrorCode =              406
+        
+	InvalidCharacteristicFormat ErrorCode =   500
+        InvalidCharacteristicValue ErrorCode =    501
+        UndefinedCharacteristic ErrorCode =       502
+        DuplicateCharacteristic ErrorCode =       503
+        MissingCharacteristic ErrorCode =         504
+        
+        UndefinedTypeCost ErrorCode =             600
+        UndefinedMatchCost ErrorCode =            601
+        UndefinedTierCost ErrorCode =             602
 	
 )
 
 // errorMsgs contains the messages associated to the error codes.
 var errorMsgs = map[ErrorCode]string{
     
-        UnusedAptitude:                "the aptitude %s is defined but not used"
-        UndefinedAptitude:             "the aptitude %s is used but not defined"
+        UnusedAptitude:                `the aptitude %s is defined but not used by the universe`,
+        UndefinedAptitude:             `the aptitude %s is used but not defined by the universe`,
     
-	InsuficientData:               "the sheet requires at least a header block and a characteristic block",
+	InvalidCharacterSheet:         `the character sheet requires at least a header block and a characteristic block`,
         
-	InvalidPairKeyValue:           "line %d: the header line is not in the proper key:value format",
-	EmptyKey:                      "line %d: the header line's key is empty",
-	EmptyValue:                    "line %d: the header line's value is empty",
-	DuplicateMeta:                 "line %d: the header's background %s is already defined",
-	InvalidOptions:                "line %d: the header's options are incorrect",
+	InvalidHeaderLine:             `line %d: the header line format is invalid`,
+	EmptyHeaderKey:                `line %d: the header line key is empty`,
+	EmptyHeaderValue:              `line %d: the header line value is empty`,
+	DuplicateBackground:           `line %d: the background is already set`,
+	InvalidBackgroundOptions:      `line %d: the background options are incorrect`,
+        UndefinedBackgroundType:       `line %d: the background %s is not defined by the universe`,
+        UndefinedBackgroundValue:      `line %d: the background %s of type %s is not defined by the universe`,
         
-	InvalidCharacteristicFormat:   "line %d: the characteristic is not properly formated",
-	NotIntegerCharacteristicValue: "line %d: the characteristic value is not an integer",
+	UndefinedSessionDate:          `line %d: the session date is not defined`,
+	InvalidSessionReward:          `line %d: the session reward is invalid`,
+	DuplicateSessionReward:        `line %d: the session reward is already set`,
+	BadSessionRewardPosition:      `line %d: bad session reward position`,
         
-	UndefinedDate:                 "line %d: the session's header contains no date",
+	InvalidUpgrade:                `line %d: the upgrade format is invalid`,
+	UndefinedUpgradeName:          `line %d: the upgrade name is not defined`,
+	InvalidUpgradeMark:            `line %d: the upgrade mark is invalid`,
+	InvalidUpgradeCost:            `line %d: the upgrade cost is invalid`,
+	DuplicateUpgradeCost:          `line %d: the upgrade cost is already set`,
+	BadUpgradeCostPosition:        `line %d: bad upgrade cost position`,
+        MismatchMarkCost:              `line %d: upgrade with mark "-" expects no cost`,
         
-	InvalidReward:                 "line %d: the session's reward is not properly set",
-	RewardAlreadyFound:            "line %d: the session has more than one reward",
-	WrongRewardPosition:           "line %d: the session's reward should be in second or last position",
+	InvalidCharacteristicFormat:   `line %d: the characteristic format is invalid`,
+        InvalidCharacteristicValue:    `line %d: the characteristic value is invalid`,
+        UndefinedCharacteristic:       `line %d: the characteristic is not defined`,
+        DuplicateCharacteristic:       `line %d: the characteristic is already set`,
+        MissingCharacteristic:         `the characteristic %s is not defined for the character`,
         
-	InvalidUpgrade:                "line %d: the upgrade's format is invalid",
-	InvalidMark:                   "line %d: upgrade's mark should be \"+\", \"-\" or \"=\"",
-	InvalidCost:                   "line %d: the upgrade's cost is not properly formated",
-	CostAlreadyFound:              "line %d: the upgrade has more than one cost",
-	WrongCostPosition:             "line %d: the upgrade's cost should be in second or last position",
-	EmptyName:                     "line %d: the upgrade has no name",
-        
-        InvalidCharacteristicCase:     "line %d: the characteristic name must be upper case"
-        InvalidCharacteristicValue:    "line %d: the characteristic value must be an integer"
-        UndefinedCharacteristic:       "line %d: the characteristic is not defined"
-        DuplicateCharacteristic:       "line %d: the characteristic is already set"
-        MissingCharacteristic:         "the characteristic %s is not defined for the character"
-        
-        UndefinedBackgroundType:       "line %d: the background type %s is not defined"
-        UndefinedBackgroundValue:      "line %d: the background %s of type %s is not defined"
-        
-        UndefinedTypeCost:             "undefined cost for type %s"
-        UndefinedMatchCost:            "undefined cost for type %s with %d matching aptitudes"
-        UndefinedTierCost:             "undefined cost for type %s with %d matching aptitudes on tier %d"
-        
-        MismatchMarkCost:              "line %d: mark "-" expects no cost value"
-        
+        UndefinedTypeCost:             `undefined cost for type %s`,
+        UndefinedMatchCost:            `undefined cost for type %s with %d matching aptitudes`,
+        UndefinedTierCost:             `undefined cost for type %s with %d matching aptitudes on tier %d`,
 }
 
 // Error is an error holding a code and variadic printable data.
